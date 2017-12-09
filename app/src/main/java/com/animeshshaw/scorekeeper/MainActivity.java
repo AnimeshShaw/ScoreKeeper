b81692fcb688aa965504af2b24e347589e52977b
AnimeshShaw <psychocoder@outlook.com>
2017-12-09 17:06:17 +0530

    initial commit

---
+++ app/src/main/java/com/animeshshaw/scorekeeper/MainActivity.java
@@ -0,0 +1,23 @@
+package com.animeshshaw.scorekeeper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void touchdownTeamA(View view) {
+    }
+
+    public void fieldGoalTeamA(View view) {
+    }
+
+    public void resetScores(View view) {
+    }
+}
